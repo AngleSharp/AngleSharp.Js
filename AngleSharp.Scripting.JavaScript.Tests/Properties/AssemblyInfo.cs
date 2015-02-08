772acb53b40b32471ef2229e58d2111a7284d92d
Florian Rappl <rappl.florian@gmail.com>
2015-02-08 20:47:13 +0100

    Added unit test project

---
+++ AngleSharp.Scripting.JavaScript.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AngleSharp.Scripting.JavaScript.Tests")]
+[assembly: AssemblyDescription("Tests for the AngleSharp.Scripting.JavaScript library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AngleVisions")]
+[assembly: AssemblyProduct("AngleSharp.Scripting")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f88920f3-8c35-4b41-9ebc-f87fd9a6caac")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
