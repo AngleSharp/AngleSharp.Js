eff7bffa075b937c45462f19291dcf86bd13b828
Florian Rappl <rappl.florian@gmail.com>
2015-04-15 21:00:29 +0200

    Updated for AngleSharp v0.8.2

---
+++ AngleSharp.Scripting.JavaScript/ScriptingService.cs
@@ -0,0 +1,17 @@
+﻿namespace AngleSharp.Scripting.JavaScript
+{
+    using AngleSharp.Network;
+    using AngleSharp.Services;
+    using System;
+
+    class ScriptingService : IScriptingService
+    {
+        public IScriptEngine GetEngine(String mimeType)
+        {
+            if (MimeTypes.IsJavaScript(mimeType))
+                return new JavaScriptEngine();
+
+            return null;
+        }
+    }
+}
