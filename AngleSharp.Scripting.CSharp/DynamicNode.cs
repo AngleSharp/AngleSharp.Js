2b9e7ccf3bda74a74c192afa5864d77721f1c2d1
Florian Rappl <rappl.florian@gmail.com>
2015-05-08 17:34:38 +0200

    Created CSharp scripting project

---
+++ AngleSharp.Scripting.CSharp/DynamicNode.cs
@@ -0,0 +1,46 @@
+﻿namespace AngleSharp.Scripting.CSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+
+    public class DynamicNode : DynamicObject
+    {
+        readonly Object _node;
+
+        public DynamicNode(Object node)
+        {
+            _node = node;
+        }
+
+        public override IEnumerable<String> GetDynamicMemberNames()
+        {
+            return base.GetDynamicMemberNames();
+        }
+
+        public override Boolean TryGetMember(GetMemberBinder binder, out Object result)
+        {
+            return base.TryGetMember(binder, out result);
+        }
+
+        public override Boolean TrySetMember(SetMemberBinder binder, Object value)
+        {
+            return base.TrySetMember(binder, value);
+        }
+
+        public override Boolean TryGetIndex(GetIndexBinder binder, Object[] indexes, out Object result)
+        {
+            return base.TryGetIndex(binder, indexes, out result);
+        }
+
+        public override Boolean TrySetIndex(SetIndexBinder binder, Object[] indexes, Object value)
+        {
+            return base.TrySetIndex(binder, indexes, value);
+        }
+
+        public override Boolean TryInvokeMember(InvokeMemberBinder binder, Object[] args, out Object result)
+        {
+            return base.TryInvokeMember(binder, args, out result);
+        }
+    }
+}
---
+++ AngleSharp.Scripting.CSharp/Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace AngleSharp.Scripting.CSharp
+{
+    using System;
+
+    /// <summary>
+    /// A set of extensions to use the C# scripting library.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Converts the provided object to a dynamic node.
+        /// </summary>
+        /// <param name="node">The node to treat dynamically.</param>
+        /// <returns>The dynamic object.</returns>
+        public static dynamic ToDynamic(this Object node)
+        {
+            return new DynamicNode(node);
+        }
+    }
+}
