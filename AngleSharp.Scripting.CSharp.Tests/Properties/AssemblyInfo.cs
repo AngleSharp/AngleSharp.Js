bc770be8ca3d4a5ba81344133d757c485c566f66
Florian Rappl <rappl.florian@gmail.com>
2015-06-29 17:24:45 -0400

    Created new test project for CSharp scripting support

---
+++ AngleSharp.Scripting.CSharp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AngleSharp.Scripting.CSharp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngleSharp")]
+[assembly: AssemblyCopyright("Copyright © AngleSharp Team 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("28e226b6-8a42-4266-a184-7e8f8b26e3d5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
