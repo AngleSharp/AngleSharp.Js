31cb95bd5c8c764a1eac64aa1326bb2f1512c18c
Florian Rappl <rappl.florian@gmail.com>
2015-07-01 14:11:34 -0400

    Referenced AngleSharp

---
+++ AngleSharp.Scripting.JavaScript.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AngleSharp.Scripting.JavaScript.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngleSharp")]
+[assembly: AssemblyCopyright("Copyright © AngleSharp Team 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("863a9ca5-a39b-42a9-967f-301d5fb8e630")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
