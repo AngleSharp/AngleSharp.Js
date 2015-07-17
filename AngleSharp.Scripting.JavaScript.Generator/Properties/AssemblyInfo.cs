ed1caebb4599b7da467f484471c6746a169166f0
Florian Rappl <rappl.florian@gmail.com>
2015-07-17 15:44:59 +0200

    Added generator

---
+++ AngleSharp.Scripting.JavaScript.Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AngleSharp.Scripting.JavaScript.Generator")]
+[assembly: AssemblyDescription("Generates the AngleSharp.Scripting.JavaScript files.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AngleVisions")]
+[assembly: AssemblyProduct("AngleSharp.Scripting")]
+[assembly: AssemblyCopyright("Copyright © AngleVisions, 2013-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c8c0eeaf-0cd3-459d-8321-bee7c7a673c0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.*")]
