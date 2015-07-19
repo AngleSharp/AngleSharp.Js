e38fe64f47077baf66094b15557ead65e2776e2a
Florian Rappl <rappl.florian@gmail.com>
2015-07-19 16:30:53 +0200

    Initial testing for the generator

---
+++ AngleSharp.Scripting.JavaScript.Tests/GeneratorTests.cs
@@ -0,0 +1,43 @@
+﻿namespace AngleSharp.Scripting.JavaScript.Tests
+{
+    using AngleSharp.Dom;
+    using AngleSharp.Scripting.JavaScript.Generator;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [TestFixture]
+    public class GeneratorTests
+    {
+        [Test]
+        public void IDocumentShouldHaveCorrectNameAndInterfacedPropertyWithNoConstructorsOrDeleters()
+        {
+            var binding = GeneralExtensions.GetBindings(new Dictionary<String, Type>
+            {
+                { "Document", typeof(IDocument) }
+            }).First();
+            Assert.AreEqual("Document", binding.Name);
+            Assert.IsTrue(binding.IsInterfaced);
+            Assert.AreEqual(0, binding.Constructors.Count());
+            Assert.AreEqual(0, binding.Deleters.Count());
+        }
+
+        [Test]
+        public void MutationObserverShouldHaveCorrectNameAndInterfacedPropertyAndTypesWithConstructor()
+        {
+            var binding = GeneralExtensions.GetBindings(new Dictionary<String, Type>
+            {
+                { "MutationObserver", typeof(MutationObserver) }
+            }).First();
+            Assert.AreEqual("MutationObserver", binding.Name);
+            Assert.IsTrue(binding.IsInterfaced);
+            Assert.AreEqual(3, binding.Members.Count());
+            Assert.AreEqual(1, binding.Constructors.Count());
+            Assert.AreEqual(0, binding.Deleters.Count());
+            Assert.IsTrue(binding.Members.Any(m => m.Key == "observe"));
+            Assert.IsTrue(binding.Members.Any(m => m.Key == "disconnect"));
+            Assert.IsTrue(binding.Members.Any(m => m.Key == "takeRecords"));
+        }
+    }
+}
