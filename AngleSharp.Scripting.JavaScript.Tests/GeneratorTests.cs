0c98fa6c427e1bac08534bfa543c9419f86e1443
Florian Rappl <rappl.florian@gmail.com>
2015-07-19 16:40:00 +0200

    More tests added

--- AngleSharp.Scripting.JavaScript.Tests/GeneratorTests.cs
+++ AngleSharp.Scripting.JavaScript.Tests/GeneratorTests.cs
@@ -11,33 +11,66 @@
     public class GeneratorTests
     {
         [Test]
-        public void IDocumentShouldHaveCorrectNameAndInterfacedPropertyWithNoConstructorsOrDeleters()
+        public void IDocumentShouldNotHaveAnyDuplicateTypes()
         {
+            var name = "Document";
             var binding = GeneralExtensions.GetBindings(new Dictionary<String, Type>
             {
-                { "Document", typeof(IDocument) }
+                { name, typeof(IDocument) }
             }).First();
-            Assert.AreEqual("Document", binding.Name);
+            Assert.AreEqual(name, binding.Name);
             Assert.IsTrue(binding.IsInterfaced);
             Assert.AreEqual(0, binding.Constructors.Count());
             Assert.AreEqual(0, binding.Deleters.Count());
         }
 
         [Test]
-        public void MutationObserverShouldHaveCorrectNameAndInterfacedPropertyAndTypesWithConstructor()
+        public void MutationObserverShouldHaveConstructor()
         {
+            var name = "MutationObserver";
+            var members = new[] { "observe", "disconnect", "takeRecords" };
             var binding = GeneralExtensions.GetBindings(new Dictionary<String, Type>
             {
-                { "MutationObserver", typeof(MutationObserver) }
+                { name, typeof(MutationObserver) }
             }).First();
-            Assert.AreEqual("MutationObserver", binding.Name);
+            Assert.AreEqual(name, binding.Name);
             Assert.IsTrue(binding.IsInterfaced);
-            Assert.AreEqual(3, binding.Members.Count());
             Assert.AreEqual(1, binding.Constructors.Count());
             Assert.AreEqual(0, binding.Deleters.Count());
-            Assert.IsTrue(binding.Members.Any(m => m.Key == "observe"));
-            Assert.IsTrue(binding.Members.Any(m => m.Key == "disconnect"));
-            Assert.IsTrue(binding.Members.Any(m => m.Key == "takeRecords"));
+            Assert.AreEqual(members.Length, binding.Members.Count());
+            
+            for (int i = 0; i < members.Length; i++)
+			{
+                Assert.IsTrue(binding.Members.Any(m => m.Key == members[i]));
+			}
+        }
+
+        [Test]
+        public void IParentNodeShouldSetInterfaceObjectCorrectly()
+        {
+            var name = "ParentNode";
+            var members = new[] 
+            { 
+                "childElementCount", "children", "firstElementChild", "lastElementChild", 
+                "append", "prepend", "querySelector", "querySelectorAll" 
+            };
+            var binding = GeneralExtensions.GetBindings(new Dictionary<String, Type>
+            {
+                { name, typeof(IParentNode) }
+            }).First();
+            Assert.AreEqual(name, binding.Name);
+            Assert.IsFalse(binding.IsInterfaced);
+            Assert.AreEqual(8, binding.Members.Count());
+            Assert.AreEqual(0, binding.Constructors.Count());
+            Assert.AreEqual(0, binding.Deleters.Count());
+            Assert.AreEqual(0, binding.Getters.Count());
+            Assert.AreEqual(0, binding.Setters.Count());
+            Assert.AreEqual(members.Length, binding.Members.Count());
+
+            for (int i = 0; i < members.Length; i++)
+            {
+                Assert.IsTrue(binding.Members.Any(m => m.Key == members[i]));
+            }
         }
     }
 }
