24f40a97dfdbeef3c6e0129319b629616a86fcc3
Florian Rappl <rappl.florian@gmail.com>
2015-07-25 14:07:58 +0200

    Added generator script

---
+++ AngleSharp.Scripting.JavaScript.Generator/run-generator.psm1
@@ -0,0 +1,41 @@
+﻿# Run in Package Manager Console:
+#
+#   cd .\AngleSharp.Scripting.JavaScript.Generator
+#   Import-Module .\run-generator.psm1 -DisableNameChecking
+#   Run-Generator
+#
+# Should do everything to create Jint Bindings
+
+function Run-Generator() {
+	Write-Host "Adding generator library ..."
+
+	$lib = "bin\Debug\AngleSharp.Scripting.JavaScript.Generator.dll"
+	$result = (Add-Type -Path $lib)
+	$project = (Get-Project "AngleSharp.Scripting.JavaScript")
+	
+	Write-Host "Checking destination folder ..."
+
+    $projectPath = [System.IO.Path]::GetDirectoryName($project.FullName)
+	$destination = (Join-Path $projectpath "Bindings")
+	$result = (New-Item -ItemType Directory -Force -Path $destination)
+
+	Write-Host "Generating JavaScript binders ..."
+
+	$config = (New-Object AngleSharp.Scripting.JavaScript.Generator.Options)
+    $files = [AngleSharp.Scripting.JavaScript.Generator.Files]::Generate($config)
+	
+	Write-Host "Saving generated files ..."
+
+	ForEach ($file in $files) {
+		$fileName = (Join-Path $destination $file.FileName)
+		$source = $file.Content
+		$result = (New-Item $fileName -ItemType "file" -Force -Value $source)
+		$result = $project.ProjectItems.AddFromFile($fileName)
+
+		Write-Host ">> Generated binding for $($file.FileName)"
+	}
+
+	Write-Host "Done."
+}
+
+Export-ModuleMember "Run-Generator"
