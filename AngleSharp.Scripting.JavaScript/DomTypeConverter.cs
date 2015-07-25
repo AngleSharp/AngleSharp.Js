3b236cbc251c537e24d34f9fade89220122e15a8
Florian Rappl <rappl.florian@gmail.com>
2015-07-25 17:29:14 +0200

    Add signature of some new converters

---
+++ AngleSharp.Scripting.JavaScript/DomTypeConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace AngleSharp.Scripting.JavaScript
+{
+    using AngleSharp.Dom;
+    using AngleSharp.Dom.Media;
+    using Jint.Native;
+    using System;
+
+    static class DomTypeConverter
+    {
+        public static DomEventHandler ToEventHandler(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IElement ToElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static INode ToNode(JsValue arg)
+        {
+            return null;
+        }
+
+        public static INode[] ToNodeArray(JsValue arg)
+        {
+            return null;
+        }
+
+        public static Action<IWindow> ToTimer(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IRenderingContext ToRenderingContext(JsValue arg)
+        {
+            return null;
+        }
+    }
+}
---
+++ AngleSharp.Scripting.JavaScript/SystemTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace AngleSharp.Scripting.JavaScript
+{
+    using Jint.Native;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    static class SystemTypeConverter
+    {
+        public static IDictionary<String, Object> ToObjBag(JsValue arg)
+        {
+            return null;
+        }
+
+        public static Action<Stream> ToStreamTask(JsValue arg)
+        {
+            return null;
+        }
+    }
+}
