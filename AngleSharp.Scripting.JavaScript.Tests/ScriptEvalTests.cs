3fe0fc6ed516a663128b22fe9e923f9c333ec5cb
Florian Rappl <rappl.florian@gmail.com>
2015-07-28 12:08:31 +0200

    Added tests for joined evaluation

---
+++ AngleSharp.Scripting.JavaScript.Tests/ScriptEvalTests.cs
@@ -0,0 +1,34 @@
+﻿namespace AngleSharp.Scripting.JavaScript.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class ScriptEvalTests
+    {
+        public static async Task<String> EvaluateComplexScriptAsync(params String[] sources)
+        {
+            var cfg = Configuration.Default.WithJavaScript();
+            var scripts = "<script>" + String.Join("</script><script>", sources) + "</script>";
+            var html = "<!doctype html><div id=result></div>" + scripts;
+            var document = await BrowsingContext.New(cfg).OpenAsync(m => m.Content(html));
+            return document.GetElementById("result").InnerHtml;
+        }
+
+        [Test]
+        public async Task AccessGlobalVariablesFromOtherScriptShouldWork()
+        {
+            var result = await EvaluateComplexScriptAsync("var a = 5;", "document.querySelector('#result').textContent = a.toString();");
+            Assert.AreEqual("5", result);
+        }
+
+        [Test]
+        public async Task AccessLocalVariablesFromOtherScriptShouldNotWork()
+        {
+            var result = await EvaluateComplexScriptAsync("(function () { var a = 5; })();", "document.querySelector('#result').textContent = a.toString();");
+            Assert.AreEqual("", result);
+        }
+    }
+}
