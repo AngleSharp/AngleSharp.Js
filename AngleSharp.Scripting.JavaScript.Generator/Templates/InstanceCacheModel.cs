68645fb26d0a856d9b86f51129940322f2232b2c
Florian Rappl <rappl.florian@gmail.com>
2015-08-02 23:23:01 +0200

    Started adding the instance cache

---
+++ AngleSharp.Scripting.JavaScript.Generator/Templates/InstanceCacheModel.cs
@@ -0,0 +1,34 @@
+﻿namespace AngleSharp.Scripting.JavaScript.Generator.Templates
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class InstanceCacheModel
+    {
+        public String Namespace
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<String> Constructors
+        {
+            get;
+            set;
+        }
+    }
+
+    partial class InstanceCache
+    {
+        public InstanceCache(InstanceCacheModel model)
+        {
+            Model = model;
+        }
+
+        public InstanceCacheModel Model
+        {
+            get;
+            private set;
+        }
+    }
+}
