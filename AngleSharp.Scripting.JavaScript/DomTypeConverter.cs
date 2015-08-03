3ea88eebf1c57c6d5ad2551bb8cb74e65f2865b5
Florian Rappl <rappl.florian@gmail.com>
2015-08-03 15:41:02 +0200

    Prototype missing converters

--- AngleSharp.Scripting.JavaScript/DomTypeConverter.cs
+++ AngleSharp.Scripting.JavaScript/DomTypeConverter.cs
@@ -1,6 +1,8 @@
 ﻿namespace AngleSharp.Scripting.JavaScript
 {
     using AngleSharp.Dom;
+    using AngleSharp.Dom.Events;
+    using AngleSharp.Dom.Html;
     using AngleSharp.Dom.Media;
     using Jint.Native;
     using System;
@@ -12,7 +14,47 @@
             return null;
         }
 
+        public static MutationCallback ToMutationCallback(JsValue arg)
+        {
+            return null;
+        }
+
         public static IElement ToElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IHtmlElement ToHtmlElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IHtmlOptionElement ToOptionElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IHtmlTableCaptionElement ToTableCaptionElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IHtmlTableSectionElement ToTableSectionElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IHtmlMenuElement ToMenuElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IHtmlOptionsGroupElement ToOptionsGroupElement(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IEventTarget ToEventTarget(JsValue arg)
         {
             return null;
         }
@@ -40,6 +82,77 @@
         public static IWindow ToWindow(JsValue arg)
         {
             return null;
+        }
+
+        public static IDocumentType ToDoctype(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IMessagePort ToMessagePort(JsValue arg)
+        {
+            return null;
+        }
+
+        public static DomException ToDomException(JsValue arg)
+        {
+            return null;
+        }
+
+        public static ITouchList ToTouchList(JsValue arg)
+        {
+            return null;
+        }
+
+        public static ITextTrackCue ToTextTrackCue(JsValue arg)
+        {
+            return null;
+        }
+
+        public static Event ToEvent(JsValue arg)
+        {
+            return null;
+        }
+
+        public static IRange ToRange(JsValue arg)
+        {
+            return null;
+        }
+
+        public static AdjacentPosition ToAdjacentPosition(JsValue arg)
+        {
+            return ToEnum<AdjacentPosition>(arg);
+        }
+
+        public static RangeType ToRangeType(JsValue arg)
+        {
+            return ToEnum<RangeType>(arg);
+        }
+
+        public static WheelMode ToWheelMode(JsValue arg)
+        {
+            return ToEnum<WheelMode>(arg);
+        }
+
+        public static MouseButton ToMouseButton(JsValue arg)
+        {
+            return ToEnum<MouseButton>(arg);
+        }
+
+        public static KeyboardLocation ToKeyboardLocation(JsValue arg)
+        {
+            return ToEnum<KeyboardLocation>(arg);
+        }
+
+        public static TextTrackMode ToTextTrackMode(JsValue arg)
+        {
+            return ToEnum<TextTrackMode>(arg);
+        }
+
+        public static T ToEnum<T>(JsValue arg)
+            where T : struct, IComparable
+        {
+ 	        return default(T);
         }
     }
 }
--- AngleSharp.Scripting.JavaScript/SystemTypeConverter.cs
+++ AngleSharp.Scripting.JavaScript/SystemTypeConverter.cs
@@ -16,5 +16,25 @@
         {
             return null;
         }
+
+        public static Nullable<Int32> ToOptionalInt32(JsValue arg)
+        {
+            return null;
+        }
+
+        public static Nullable<DateTime> ToOptionalDateTime(JsValue arg)
+        {
+            return null;
+        }
+
+        public static Object ToObject(JsValue arg)
+        {
+            return null;
+        }
+
+        public static String[] ToStringArray(JsValue arg)
+        {
+            return null;
+        }
     }
 }
