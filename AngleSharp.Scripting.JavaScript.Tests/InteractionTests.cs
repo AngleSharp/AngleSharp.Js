0c30ac0dc95e99bede345c24dfde5898067628eb
Florian Rappl <rappl.florian@gmail.com>
2015-08-25 21:11:01 +0200

    Add interaction tests, #9

---
+++ AngleSharp.Scripting.JavaScript.Tests/InteractionTests.cs
@@ -0,0 +1,35 @@
+﻿namespace AngleSharp.Scripting.JavaScript.Tests
+{
+    using Jint.Runtime;
+    using NUnit.Framework;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class InteractionTests
+    {
+        [Test]
+        public async Task ReadStoredJavaScriptValueFromCSharp()
+        {
+            var service = new ScriptingService();
+            var cfg = Configuration.Default.With(service);
+            var html = "<!doctype html><script>var foo = 'test';</script>";
+            var document = await BrowsingContext.New(cfg).OpenAsync(m => m.Content(html));
+            var foo = service.Engine.GetJint(document).GetValue("foo");
+            Assert.AreEqual(Types.String, foo.Type);
+            Assert.AreEqual("test", foo.AsString());
+        }
+
+        [Test]
+        public async Task RunJavaScriptFunctionFromCSharp()
+        {
+            var service = new ScriptingService();
+            var cfg = Configuration.Default.With(service);
+            var html = "<!doctype html><script>function square(x) { return x * x; }</script>";
+            var document = await BrowsingContext.New(cfg).OpenAsync(m => m.Content(html));
+            var square = service.Engine.GetJint(document).GetValue("square");
+            var result = square.Invoke(4);
+            Assert.AreEqual(Types.Number, result.Type);
+            Assert.AreEqual(16.0, result.AsNumber());
+        }
+    }
+}
