d0334d9c63fa5b8f327bd442bbedab8e9d75f7a4
Florian Rappl <rappl.florian@gmail.com>
2015-08-25 21:52:50 +0200

    Test for trivial extensibility #3

--- AngleSharp.Scripting.JavaScript.Tests/InteractionTests.cs
+++ AngleSharp.Scripting.JavaScript.Tests/InteractionTests.cs
@@ -2,6 +2,7 @@
 {
     using Jint.Runtime;
     using NUnit.Framework;
+    using System;
     using System.Threading.Tasks;
 
     [TestFixture]
@@ -30,6 +31,46 @@
             var result = square.Invoke(4);
             Assert.AreEqual(Types.Number, result.Type);
             Assert.AreEqual(16.0, result.AsNumber());
+        }
+
+        [Test]
+        public async Task RunCSharpFunctionFromJavaScript()
+        {
+            var service = new ScriptingService();
+            var cfg = Configuration.Default.With(service);
+            var storedValue = 0.0;
+            service.Engine.External["square"] = new Action<Double>(x => storedValue = x);
+            var html = "<!doctype html><script>square(4 * 4);</script>";
+            var document = await BrowsingContext.New(cfg).OpenAsync(m => m.Content(html));
+            Assert.AreEqual(16.0, storedValue);
+        }
+
+        [Test]
+        public async Task AccessCSharpInstanceMembersFromJavaScript()
+        {
+            var service = new ScriptingService();
+            var cfg = Configuration.Default.With(service);
+            service.Engine.External["person"] = new Person { Age = 20, Name = "Foobar" };
+            var html = "<!doctype html><script>var str = person.Name + ' is ' + person.Age + ' years old';</script>";
+            var document = await BrowsingContext.New(cfg).OpenAsync(m => m.Content(html));
+            var str = service.Engine.GetJint(document).GetValue("str");
+            Assert.AreEqual(Types.String, str.Type);
+            Assert.AreEqual("Foobar is 20 years old", str.AsString());
+        }
+
+        class Person
+        {
+            public String Name
+            {
+                get;
+                set;
+            }
+
+            public Int32 Age
+            {
+                get;
+                set;
+            }
         }
     }
 }
