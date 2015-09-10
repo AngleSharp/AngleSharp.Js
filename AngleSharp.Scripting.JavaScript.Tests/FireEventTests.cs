c5fe69cc40d7d96ab8b2a328e0d8b79814de6bee
Florian Rappl <rappl.florian@gmail.com>
2015-09-10 10:09:49 +0200

    Test event combination

--- AngleSharp.Scripting.JavaScript.Tests/FireEventTests.cs
+++ AngleSharp.Scripting.JavaScript.Tests/FireEventTests.cs
@@ -33,5 +33,45 @@
             document.Dispatch(new CustomEvent("hello"));
             Assert.AreEqual("2", div.TextContent);
         }
+
+        [Test]
+        public async Task InvokeLoadEventFromJsAndCustomEventFromJsAndCs()
+        {
+            var service = new ScriptingService();
+            var cfg = Configuration.Default.With(service);
+            var html = @"<!doctype html>
+<html>
+<body>
+<script>
+var log = [];
+log.push('1');
+document.addEventListener('load', function() {
+    log.push('5');
+}, false);
+document.addEventListener('hello', function() {
+    log.push('3');
+}, false);
+log.push('2');
+</script>
+</body>";
+            var document = await BrowsingContext.New(cfg).OpenAsync(m => m.Content(html));
+            var log = service.Engine.GetJint(document).GetValue("log").AsArray();
+            document.AddEventListener("hello", (s, ev) =>
+            {
+                log.Put(log.Get("length").AsNumber().ToString(), "4", false);
+            });
+
+            var e = document.CreateEvent("event");
+            e.Init("hello", false, false);
+            document.Dispatch(e);
+            
+            await Task.Delay(50);
+            Assert.AreEqual(5.0, log.Get("length").AsNumber());
+            Assert.AreEqual("1", log.Get("0").AsString());
+            Assert.AreEqual("2", log.Get("1").AsString());
+            Assert.AreEqual("5", log.Get("2").AsString());
+            Assert.AreEqual("3", log.Get("3").AsString());
+            Assert.AreEqual("4", log.Get("4").AsString());
+        }
     }
 }
