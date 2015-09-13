043e0de2eb97ce591df6aaa921eae56a2cc7496f
Florian Rappl <rappl.florian@gmail.com>
2015-09-13 12:57:08 +0200

    Added real world pages as tests

---
+++ AngleSharp.Scripting.JavaScript.Tests/PageTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AngleSharp.Scripting.JavaScript.Tests
+{
+    using NUnit.Framework;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class PageTests
+    {
+        //[Test]
+        public async Task RunHtml5Test()
+        {
+            var target = "http://html5test.com";
+            var config = Configuration.Default.WithJavaScript().WithCss().WithDefaultLoader(setup => setup.IsResourceLoadingEnabled = true);
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(target);
+            var points = document.QuerySelector("#score > .pointsPanel > h2 > strong").TextContent;
+            Assert.AreNotEqual("0", points);
+        }
+
+        //[Test]
+        public async Task RunTaobao()
+        {
+            var target = "https://meadjohnson.world.tmall.com/search.htm?search=y&orderType=defaultSort&scene=taobao_shop";
+            var config = Configuration.Default.WithJavaScript().WithCss().WithDefaultLoader(setup => setup.IsResourceLoadingEnabled = true);
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(target);
+            var prices = document.QuerySelectorAll("span.c-price");
+            Assert.AreNotEqual(0, prices.Length);
+        }
+    }
+}
