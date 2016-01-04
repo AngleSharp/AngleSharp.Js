13dee7483fb61ee507f9d74376f99a9b46097b94
Florian Rappl <rappl.florian@gmail.com>
2016-01-04 23:35:41 +0100

    Added test to verify #20

---
+++ AngleSharp.Scripting.JavaScript.Tests/IntegrationTests.cs
@@ -0,0 +1,18 @@
+﻿namespace AngleSharp.Scripting.JavaScript.Tests
+{
+    using NUnit.Framework;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class IntegrationTests
+    {
+        [Test]
+        public async Task PageEcoEnergyShouldLoadFine()
+        {
+            var config = Configuration.Default.WithDefaultLoader().WithCss().WithCookies().WithJavaScript();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync("http://www.eco2energie.biz/");
+            Assert.IsNotNull(document);
+        }
+    }
+}
