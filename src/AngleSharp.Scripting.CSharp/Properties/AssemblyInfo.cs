29aa5b8b5b0f826e9176f9f4eaf3706f02c27dc7
Florian Rappl <rappl.florian@gmail.com>
2016-07-17 09:24:35 +0200

    Adjusted version number

--- src/AngleSharp.Scripting.CSharp/Properties/AssemblyInfo.cs
+++ src/AngleSharp.Scripting.CSharp/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.1.*")]
-[assembly: AssemblyFileVersion("0.1.6")]
+[assembly: AssemblyVersion("0.2.0.*")]
+[assembly: AssemblyFileVersion("0.2.0")]
 [assembly: InternalsVisibleToAttribute("AngleSharp.Scripting.CSharp.Tests")]
