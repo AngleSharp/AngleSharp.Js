0412f883e89924727f9e0689d38cce3c28d0d7bd
Georgios Diamantopoulos <georgiosd@gmail.com>
2017-05-09 07:27:29 +0300

    make prototype cache thread-safe

--- src/AngleSharp.Scripting.JavaScript/PrototypeCache.cs
+++ src/AngleSharp.Scripting.JavaScript/PrototypeCache.cs
@@ -1,4 +1,6 @@
-﻿namespace AngleSharp.Scripting.JavaScript
+﻿using System.Collections.Concurrent;
+
+namespace AngleSharp.Scripting.JavaScript
 {
     using Jint;
     using Jint.Native.Object;
@@ -7,27 +9,21 @@
 
     sealed class PrototypeCache
     {
-        private readonly Dictionary<Type, ObjectInstance> _prototypes;
+        private readonly ConcurrentDictionary<Type, ObjectInstance> _prototypes;
         private readonly Engine _engine;
 
         public PrototypeCache(Engine engine)
         {
-            _prototypes = new Dictionary<Type, ObjectInstance>();
-            _prototypes.Add(typeof(Object), engine.Object.PrototypeObject);
+            _prototypes = new ConcurrentDictionary<Type, ObjectInstance>
+            {
+                [typeof(Object)] = engine.Object.PrototypeObject
+            };
             _engine = engine;
         }
 
         public ObjectInstance GetOrCreate(Type type, Func<Type, ObjectInstance> creator)
         {
-            var instance = default(ObjectInstance);
-
-            if (!_prototypes.TryGetValue(type, out instance))
-            {
-                instance = creator.Invoke(type);
-                _prototypes.Add(type, instance);
-            }
-
-            return instance;
+            return _prototypes.GetOrAdd(type, creator.Invoke);
         }
     }
 }
