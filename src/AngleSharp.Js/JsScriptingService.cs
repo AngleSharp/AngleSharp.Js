b99ff2ab7c220c2cf020c79d7666fe4e79fb41ee
Florian Rappl <florian.rappl@smapiot.com>
2019-05-24 00:53:50 +0200

    Use new AngleSharp exposed feature

--- src/AngleSharp.Js/JsScriptingService.cs
+++ src/AngleSharp.Js/JsScriptingService.cs
@@ -76,34 +76,12 @@ namespace AngleSharp.Scripting
         public async Task EvaluateScriptAsync(IResponse response, ScriptOptions options, CancellationToken cancel)
         {
             var encoding = options.Encoding ?? Encoding.UTF8;
-            var context = options.Document.Context;
-            var loop = context.GetService<IEventLoop>();
 
             using (var reader = new StreamReader(response.Content, encoding, true))
             {
                 var content = await reader.ReadToEndAsync().ConfigureAwait(false);
-
-                if (loop != null)
-                {
-                    var tcs = new TaskCompletionSource<Boolean>();
-                    loop.Enqueue(_ =>
-                    {
-                        try
-                        {
-                            EvaluateScript(options.Document, content);
-                            tcs.SetResult(true);
-                        }
-                        catch (Exception ex)
-                        {
-                            tcs.SetException(ex);
-                        }
-                    }, TaskPriority.Critical);
-                    await tcs.Task.ConfigureAwait(false);
-                }
-                else
-                {
-                    EvaluateScript(options.Document, content);
-                }
+                await options.EventLoop.EnqueueAsync(_ =>
+                    EvaluateScript(options.Document, content), TaskPriority.Critical).ConfigureAwait(false);
             }
         }
 
