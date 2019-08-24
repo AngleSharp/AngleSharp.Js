1a1207ec542d2b31a9cdd0d6edf4a02cb0e315b6
Florian Rappl <mail@florian-rappl.de>
2019-08-24 09:43:55 +0200

    Added more tests

--- src/AngleSharp.Js.Tests/JavascriptErrorTests.cs
+++ src/AngleSharp.Js.Tests/JavascriptErrorTests.cs
@@ -29,5 +29,31 @@ namespace AngleSharp.Js.Tests
 
             await context.OpenAsync(r => r.Content(content));
         }
+
+        [Test]
+        public async Task CanComputeAndHoistLocalVariablesInForLoop()
+        {
+            var source = @"
+(function (cl) {
+          for (
+            var n = !!document.getElementsByClassName,
+              ret = [],
+              els = n
+                ? document.getElementsByClassName(cl)
+                : document.getElementsByTagName(""*""),
+              p = n ? false : new RegExp(""(^|\\s)"" + cl + ""(\\s|$)""),
+              i = 0;
+            i < els.length;
+            i++
+          )
+            if (!p || p.test(els[i].className)) ret.push(els[i]);
+          return ret;
+})('abc').length";
+            var config = Configuration.Default
+                .WithJs();
+            var document = await BrowsingContext.New(config).OpenNewAsync();
+            var result = document.ExecuteScript(source);
+            Assert.AreEqual(0.0, result);
+        }
     }
 }
--- src/AngleSharp.Js.Tests/PageTests.cs
+++ src/AngleSharp.Js.Tests/PageTests.cs
@@ -11,32 +11,24 @@ namespace AngleSharp.Js.Tests
     {
         private static Task<IDocument> LoadPage(String url)
         {
-            var configuration = Helpers.GetCssConfig().WithDefaultLoader(new LoaderOptions { IsResourceLoadingEnabled = true });
+            var configuration = Helpers.GetCssConfig()
+                .WithDefaultLoader(new LoaderOptions { IsResourceLoadingEnabled = true });
             var context = BrowsingContext.New(configuration);
             return context.OpenAsync(url);
         }
 
-        //[Test]
+        [Test]
         public async Task RunHtml5Test()
         {
             if (Helpers.IsNetworkAvailable())
             {
-                var target = "http://html5test.com";
+                var target = "https://html5test.com";
                 var document = await LoadPage(target);
-                var points = document.QuerySelector("#score > .pointsPanel > h2 > strong").TextContent;
+                await document.WaitForReadyAsync();
+                var result = document.QuerySelector("#score > .pointsPanel > h2 > strong");
+                Assert.IsNotNull(result);
+                var points = result?.TextContent ?? "0";
                 Assert.AreNotEqual("0", points);
-            }
-        }
-
-        //[Test]
-        public async Task RunTaobao()
-        {
-            if (Helpers.IsNetworkAvailable())
-            {
-                var target = "https://meadjohnson.world.tmall.com/search.htm?search=y&orderType=defaultSort&scene=taobao_shop";
-                var document = await LoadPage(target);
-                var prices = document.QuerySelectorAll("span.c-price");
-                Assert.AreNotEqual(0, prices.Length);
             }
         }
     }
